5830d748a8661b435e4cecc2543b7c5601c317e9
Rajat Yadav <myselfrajatyadav@gmail.com>
2025-05-08 12:23:43 +0530

    refactor: replace hardcoded value with environment variable for origin option in Express CORS middleware

--- rent-your-place-backend/index.js
+++ rent-your-place-backend/index.js
@@ -7,18 +7,15 @@ dotenv.config({ path: [".env.production", ".env.example", ".env"] });
 
 const app = express();
 
-app.use(express.json());
-
 app.use(cors({
-    origin: ["https://rajatyadav01.github.io", "http://127.0.0.1:3000"],
+    origin: [process.env.FRONTEND_HOST_URL],
     credentials: true
 }));
+app.use(express.json());
+app.use("/api/users", require("./routes/user-routes"));
+app.use("/api/properties", require("./routes/property-routes"));
 
 databaseSetup();
-
-app.use("/api/users", require("./routes/user-routes"));
-
-app.use("/api/properties", require("./routes/property-routes"));
 
 const PORT = process.env.PORT || process.env.ALTERNATIVE_PORT;
 
